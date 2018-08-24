6900a18cbabd848e7e6359e13a4831d4d4436ec4
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-08-24 15:58:54 -0500

    edits from tutoring session

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,8 +19,8 @@
 // Congratulate the player on guessing the color
 
 // fun little welcome message
-var name = prompt("Hi, colorful. What's your name?");
-alert("Hello " + name);
+// var name = prompt("Hi, colorful. What's your name?");
+// alert("Hello " + name);
 
 // all the colors array, should be 140
 var allColors = [
@@ -41,65 +41,85 @@ var allColors = [
     "silver", "darkgray", "gray", "dimgray", "lightslategray", "slategray", "darkslategray", "black"
 ];
 
+var alphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphabet = alphabetArray();
+
 // variables to hold the number of wins, losses and remaining letters.
 var wins = 0;
 var losses = 0;
-var remainingLetters = color.length;
 
 // Create variables that hold references to the places in the HTML where we want to display things.
-var directionsText = document.getElementById("directions-text");
-var userGuessText = document.getElementById("userguess-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
+var inputText = document.getElementById("inputText"); // letters the user enters
+var winsText = document.getElementById("wins-text"); // the number of times a user answers a whole color correctly
+var lossesText = document.getElementById("losses-text"); // number of times the user has lost, ran out of guesses
+var guessesRemaining = document.getElementById("guesses-remaining"); // number of single-letter guesses remaining, always 10
+var lettersGuessed = document.getElementById("letters-guessed"); // listing of letters user entered but not part of word, already used
+var currentWord = document.getElementById("current-word");
 
-// computer chooses random color to play when user loads page
+// STEP 1) computer chooses our random color to play
+var color = allColors[Math.floor(Math.random() * allColors.length)];
+console.log(color);
 
+// STEP 2) display underscores for the color to be played/guessed
+var answerArray = [];
+for (var i = 0; i < color.length; i++) {
+    answerArray[color.length] = "_ ";
+    console.log(answerArray[color.length]);
+}
+var remainingLetters = color.length;
+
+console.log(answerArray.join(" "));
 
 // This function is run whenever the user presses the guess button after entering a letter.
-document.onclick = function (event) {
-    // computer chooses our random color to play
-    var color = allColors[Math.floor(Math.random() * allColors.length)];
-    // Determines which key was pressed by the user.
-    var guess = event.key;
-    // Only run the following code block if the user keys a single letter.    
-    if (guess.length !== 1) {
-        alert("Please enter a single letter.");
-    } else {
-        // run block to determine if guessed letter is in the color. If so, print letter, deduct from remaining letters, increase wins
-        for (var j = 0; j < color.length; j++) {
-            if (color[j] === guess) {
-                answerArray[j] = guess;
-                remainingLetters--;
-                wins++;
-            }
-        }
+// document.getElementById("guessBtn").onclick = function (event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     // computer chooses our random color to play
+//     var color = allColors[Math.floor(Math.random() * allColors.length)];
+//     // variable to capture remanining letters to use in loop
+//     var remainingLetters = color.length;
+//     // Determines which key was pressed by the user.
+//     var guess = document.getElementById("inputText").value;
+//     console.log("guess " + guess);
 
-        // our underscores for the color to be played
-        var answerArray = [];
-        for (var i = 0; i < color.length; i++) {
-            answerArray[i] = "_";
-        }
+//     // run block to determine if guessed letter is in the color. If so, print letter, deduct from remaining letters, increase wins
+//     for (var j = 0; j < color.length; j++) {
+//         if (color[j] === guess) {
+//             answerArray[j] = guess;
+//             remainingLetters--;
+//             wins++;
+//         }
+//     }
 
-        // the game loop
-        while (remainingLetters > 0) {
-            // shows the player's progress aka joins the guess and the underscores / displays letters
-            alert(answerArray.join(" "));
-        }
+//      our underscores for the color to be played
+//      var answerArray = [];
+//      for (var i = 0; i < color.length; i++) {
+//          answerArray[i] = "_";
+//      }
 
-        if (guess === null) {
-            // exit the game loop
-            break;
-        } else if (guess.length !== 1) {
-            alert("Please enter a single letter.");
-        } else {
-            // update game status with the guess
-            for (var j = 0; j < color.length; j++) {
-                if (color[j] === guess) {
-                    answerArray[j] = guess;
-                    remainingLetters--;
-                }
-            }
-        }
-        // show answer and congratulate player
-        alert(answerArray.join(" "));
-        alert("Good job! The answer was " + word);
+//     // the game loop
+//     while (remainingLetters > 0) {
+//         // shows the player's progress aka joins the guess and the underscores / displays letters
+//         alert(answerArray.join(" "));
+//     }
+
+//     if (guess === null) {
+//         // exit the game loop
+//         break;
+//     } else if (guess.length !== 1) {
+//         alert("Please enter a single letter.");
+//     } else {
+//         // update game status with the guess
+//         for (var j = 0; j < color.length; j++) {
+//             if (color[j] === guess) {
+//                 answerArray[j] = guess;
+//                 remainingLetters--;
+//             }
+//         }
+//     }
+//     // show answer and congratulate player
+//     // alert(answerArray.join(" "));
+//     // alert("Good job! The answer was " + word);
+
+//     winsText.textContent = "wins: " + wins;
+//     lossesText.textContent = "losses: " + losses;
